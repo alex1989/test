1e0f44d55cb7fe82892b7d6a13269a98a5e6c389
Alex <alex2007v@gmail.com>
2014-08-19 15:58:20 +0900

    add unique URLs for each list

--- lists/views.py
+++ lists/views.py
@@ -7,11 +7,12 @@ def home_page(request):
         return redirect('/lists/the-only-list-in-the-world/')
     return render(request, 'home.html')
 
-def view_list(request):
-    items = Item.objects.all()
+def view_list(request, list_id):
+    lst = List.objects.get(id=list_id)
+    items = Item.objects.filter(list=lst)
     return render(request, 'list.html', {'items': items})
 
 def new_list(request):
     lst = List.objects.create()
     Item.objects.create(text=request.POST['item_text'], list=lst)
-    return redirect('/lists/the-only-list-in-the-world/')
+    return redirect('/lists/%d/' % (lst.id,))
