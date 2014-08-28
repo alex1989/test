dca3bba039f99492c44983b7f801cef31d60a69c
Alex <alex2007v@gmail.com>
2014-08-28 16:51:39 +0900

    added some functional test and unit tests for new forms, and modification in models

--- lists/forms.py
+++ lists/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from lists.models import Item
 
 EMPTY_LIST_ERROR = "You can't have an empty list item"
+DUPLICATE_ITEM_ERROR = "You've already got this in your list"
 
 class ItemForm(forms.ModelForm):
 
@@ -21,3 +22,19 @@ class ItemForm(forms.ModelForm):
     def save(self, for_list):
         self.instance.list = for_list
         return super().save()
+
+class ExistingListItemForm(ItemForm):
+
+    def __init__(self, for_list, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.instance.list = for_list
+
+    def validate_unique(self):
+        try:
+            self.instance.validate_unique()
+        except forms.ValidationError as e:
+            e.error_dict = {'text': [DUPLICATE_ITEM_ERROR]}
+            self._update_errors(e)
+
+    def save(self):
+        return forms.models.ModelForm.save(self)
